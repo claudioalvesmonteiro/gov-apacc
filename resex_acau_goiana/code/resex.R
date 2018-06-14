b70561f7bf7284de23a436dd7d9c30b4d6dc0aa4
claudioalvesmonteiro <claudiomonteiro@protonmail.com>
2018-06-14 17:37:31 -0300

    commit text resex init

--- resex_acau_goiana/code/resex.R
+++ resex_acau_goiana/code/resex.R
@@ -1,18 +1,35 @@
-#================================================#
-# PIBIC Claudio Monteiro                         #        
-# RESEX ACAU-GOIANA                              #
-#================================================#
-# ANALISE DE CONTEUDO DAS MEMORIAS               #
-# Recife - Pernambuco - Brasil                   #
-#------------------------------------------------#
-# Fundacaoo Joaquim Nabuco (FUNDAJ)              #
-#------------------------------------------------#
-# Claudio A. Monteiro                            #
-# claudiomonteirol.a@gmail.com                   #
-#================================================#
- 
+#=====================================================================#
+# DECISAO E PARTICIPACAO NO CONSELHO GESTOR DA RESEX ACAU-GOIANA      #
+#=====================================================================#
+# Analise da participacao - Assentos/Frequencia/Voz                   #
+# Recife - Pernambuco - Brasil                                        #
+#---------------------------------------------------------------------#
+# Fundacao Joaquim Nabuco (FUNDAJ)                                    #
+#---------------------------------------------------------------------#
+# Claudio A. Monteiro                                                 #
+# claudiomonteirol.a@gmail.com                                        #
+#=====================================================================# 
+
 # carregar pacotes
-library(stringi)
+library(GGally); library(network); library(sna); library(ggplot2); library(RQDA); library(readxl)
+library(RQDA); library(dplyr); library(stringr); library(ggplot2); library(networkD3)
+
+# tema para os graficos em ggplot
+tema_massa <- function(base_size = 12, base_family = "") {
+  theme_minimal(base_size = base_size, base_family = base_family) %+replace% 
+    theme(axis.text.x = element_text(colour= "black",size=11,hjust=.5,vjust=.5,face="plain"),
+          axis.text.y = element_text(colour="black",size=11,angle=0,hjust=1,vjust=0,face="plain"), 
+          axis.title.x = element_text(colour="black",size=14,angle=0,hjust=.5,vjust=0,face="plain"),
+          axis.title.y = element_text(colour="black",size=13,angle=90,hjust=0.5,vjust=0.6,face="plain"),
+          title = element_text(colour="black",size=12,angle=0,hjust=.5,vjust=.5,face="plain"),
+          panel.grid.major = element_line(colour = grey(0.85)), 
+          panel.grid.minor = element_line(colour = grey(1)),
+          legend.key.size = unit(9, "mm"),
+          legend.text = element_text(size = 9, hjust = 3, vjust = 3),
+          legend.title = element_text(size = 9),
+          axis.line = element_line(size = 1, colour = "grey70"))
+}
+
 
 #===== Tranformar PDF em .txt =====#
 
